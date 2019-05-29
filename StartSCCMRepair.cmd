d3bba0cf2ff8b818f0477e990ab98f9031283171
Steven Bart <46748827+stevenbartcom@users.noreply.github.com>
2019-05-29 13:21:41 +0200

    Update StartSCCMRepair.cmd

--- StartSCCMRepair.cmd
+++ StartSCCMRepair.cmd
@@ -3,8 +3,9 @@ REM ==================================================
 REM NAME: SCCMRepair.ps1
 REM AUTHOR: Steven Bart, https://stevenbart.com
 REM
-REM VERSION: 1811.0
+REM VERSION: 1905
 REM COMMENTS: This script start the SCCMRepair.ps1 file
+REM LICENSE: MIT License
 REM ===================================================
 @echo on
 powershell.exe -executionpolicy bypass -command .\SCCMRepair.ps1
